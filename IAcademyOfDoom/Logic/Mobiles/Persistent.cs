130cbb70c47569accd641ecd49ac1af6ab9718fc
FuzniX <dupontnicolas703@gmail.com>
2025-04-18 00:03:29 +0200

    Coded all botling types

--- IAcademyOfDoom/Logic/Mobiles/Persistent.cs
+++ IAcademyOfDoom/Logic/Mobiles/Persistent.cs
@@ -1,14 +1,26 @@
-﻿namespace IAcademyOfDoom.Logic.Mobiles
+﻿using System;
+using System.Collections.Generic;
+using IAcademyOfDoom.Logic.Places;
+
+namespace IAcademyOfDoom.Logic.Mobiles
 {
     public class Persistent : Botling
     {
+        private bool HadFailed = false;
         public Persistent() : base(BotType.Persistent) {}
 
         public override ExamResult Exam()
         {
             ExamResult result = base.Exam();
+            HadFailed = result == ExamResult.Failure;
             if (result == ExamResult.Failure) (X, Y) = (0, 0);
             return result;
+        }
+
+        protected override (int x, int y) Next(List<Room> rooms, List<Direction> directions = null)
+        {
+            if (HadFailed) {HadFailed = false; return (0, 0);}
+            return base.Next(rooms, directions);
         }
     }
 }
--- IAcademyOfDoom/Logic/Mobiles/Speedster.cs
+++ IAcademyOfDoom/Logic/Mobiles/Speedster.cs
@@ -8,35 +8,26 @@ namespace IAcademyOfDoom.Logic.Mobiles
     {
         public Speedster() : base(BotType.Speedster) {}
         
-        protected override (int x, int y) Next(List<Room> rooms)
+        protected override (int x, int y) Next(List<Room> rooms, List<Direction> directions = null)
         {
-            int length = 0;
-
+            int length = 1;
             try
             {
+                Direction bestDirection = BestDirection(rooms, directions);
                 while (true)
                 {
-                    if (Orientation)
-                    {
-                        switch (BestDirection(X + length, Y + length, rooms))
-                        {
-                            case 0: return (X + length, Y);
-                            case 1: return (X, Y + length);
-                            case -2: return Game.Random.Next() %2 == 0 ? (X + length, Y) : (X, Y + length);
-                        }
-                    }
-                    else
-                    {
-                        if (Game.FindRoomAt(X + length, Y, rooms) != null) return (X + length, Y);
-                        if (Game.FindRoomAt(X, Y + length, rooms) != null) return (X, Y + length);
-                    }
-                    
-                    if (X + length >= Game.MaxX && X < Game.MaxX) return (X + length, Y);
-                    if (Y + length >= Game.MaxY && Y < Game.MaxY) return (X, Y + length);
+                    (int x, int y) = Next(bestDirection, length);
+                    if ((x, y) == (Game.MaxX, Game.MaxY) || Game.FindRoomAt(x, y, rooms) is ProfRoom) return (x, y);
+                    if (IsOutOfBounds(x, y)) {length--; return Next(bestDirection, length);}
                     length++;
                 }
             }
             finally { if (length > 1) HP--; } // Decrease HP if, whatever it is, the move is of length 2 or more
+        }
+
+        protected static bool IsOutOfBounds(int x, int y)
+        {
+            return x < 0 || x > Game.MaxX || y < 0 || y > Game.MaxY;
         }
     }
 }
