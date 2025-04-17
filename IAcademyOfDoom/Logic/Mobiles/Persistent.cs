241660965090a8c5fccbb5d543a843c5ac6fff93
FuzniX <dupontnicolas703@gmail.com>
2025-04-18 00:39:41 +0200

    respecting naming rules

--- IAcademyOfDoom/Logic/Mobiles/Persistent.cs
+++ IAcademyOfDoom/Logic/Mobiles/Persistent.cs
@@ -6,20 +6,20 @@ namespace IAcademyOfDoom.Logic.Mobiles
 {
     public class Persistent : Botling
     {
-        private bool HadFailed;
+        private bool hadFailed;
         public Persistent() : base(BotType.Persistent) {}
 
         public override ExamResult Exam()
         {
             ExamResult result = base.Exam();
-            HadFailed = result == ExamResult.Failure;
+            hadFailed = result == ExamResult.Failure;
             if (result == ExamResult.Failure) (X, Y) = (0, 0);
             return result;
         }
 
         public override (int x, int y) Next(List<Room> rooms, List<Direction> directions = null)
         {
-            if (HadFailed) {HadFailed = false; return (0, 0);}
+            if (hadFailed) {hadFailed = false; return (0, 0);}
             return base.Next(rooms, directions);
         }
     }
