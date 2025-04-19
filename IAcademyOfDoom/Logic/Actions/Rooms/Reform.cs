98ad2d6aa92e745e9b217c1549c4b291cdd115bf
Nicolas Dupont <nicolas.dupont.1@etu.u-bordeaux.fr>
2025-04-19 15:41:14 +0200

    Implemented

--- IAcademyOfDoom/Logic/Actions/Rooms/Reform.cs
+++ IAcademyOfDoom/Logic/Actions/Rooms/Reform.cs
@@ -1,17 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using IAcademyOfDoom.Logic.Places;
 
 namespace IAcademyOfDoom.Logic.Actions.Rooms
 {
     public class Reform : Action
     {
         public const int Amount = 1;
-        public Reform(string name) : base(name)
+        
+        private readonly List<Room> rooms;
+        
+        public Reform(string name, List<Room> rooms) : base(name)
         {
+            this.rooms = rooms;
         }
 
         public override void Execute()
         {
-            throw new NotImplementedException();
+            rooms.ForEach(room =>
+            {
+                if ((room.X != 0 && room.Y != 0) && (room.X != Game.MaxX && room.Y != Game.MaxY))
+                {
+                    while (true)
+                    {
+                        (int x, int y) = (Game.Random.Next(0, Game.MaxX), Game.Random.Next(0, Game.MaxY));
+                        if (Game.FindRoomAt(x, y, rooms) == null)
+                        {
+                            room.X = x;
+                            room.Y = y;
+                        }
+                    }
+                }
+            });
         }
     }
 }
