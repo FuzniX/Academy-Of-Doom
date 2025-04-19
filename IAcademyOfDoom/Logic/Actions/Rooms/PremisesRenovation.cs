6211cf79b672bd9ebea0973c29bcb57e80f68384
FuzniX <dupontnicolas703@gmail.com>
2025-04-19 04:22:57 +0200

    Actions skeletons

---
+++ IAcademyOfDoom/Logic/Actions/Rooms/PremisesRenovation.cs
@@ -0,0 +1,20 @@
+﻿using IAcademyOfDoom.Logic.GameSettings;
+using IAcademyOfDoom.Logic.Places;
+
+namespace IAcademyOfDoom.Logic.Actions.Rooms
+{
+    public class PremisesRenovation : Action
+    {
+        private readonly ProfRoom profRoom;
+        
+        public PremisesRenovation(string name, ProfRoom profRoom) : base(name)
+        {
+            this.profRoom = profRoom;
+        }
+        
+        public override void Execute()
+        {
+            profRoom.HP = Default.BaseProfHitPoints;
+        }
+    }
+}
---
+++ IAcademyOfDoom/Logic/Actions/Rooms/Prof/FigureVisit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using IAcademyOfDoom.Logic.Places;
+
+namespace IAcademyOfDoom.Logic.Actions.Rooms.Prof
+{
+    public class FigureVisit : AllProfRoomsAction
+    {
+        public static readonly int Amount = 3;
+        
+        public FigureVisit(string name, List<ProfRoom> profRooms) : base(name, profRooms)
+        {
+        }
+
+        public override void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
