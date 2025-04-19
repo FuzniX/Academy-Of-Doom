3f3a54f88543cccfbe292765c5507e87c2f43140
Nicolas Dupont <nicolas.dupont.1@etu.u-bordeaux.fr>
2025-04-19 15:02:03 +0200

    Implemented

--- IAcademyOfDoom/Logic/Actions/Botlings/Corruption.cs
+++ IAcademyOfDoom/Logic/Actions/Botlings/Corruption.cs
@@ -6,14 +6,25 @@ namespace IAcademyOfDoom.Logic.Actions.Botlings
     public class Corruption : AbstractBotlingsAction
     {
         public const int Amount = 1;
+
+        private const int HpToLose = 3;
+
+        private const int MoneyPerDeath = 1;
         
-        public Corruption(string name, List<Botling> botlings) : base(name, botlings)
+        private readonly Game game;
+        
+        public Corruption(string name, List<Botling> botlings, Game game) : base(name, botlings)
         {
+            this.game = game;
         }
 
         public override void Execute()
         {
-            throw new System.NotImplementedException();
+            Botlings.ForEach(botling =>
+            {
+                ChangeBotlingHp(botling, HpToLose);
+                if (botling.HP <= 0) game.AddMoney(MoneyPerDeath);
+            });
         }
     }
 }
